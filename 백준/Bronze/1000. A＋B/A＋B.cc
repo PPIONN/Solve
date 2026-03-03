0ec70319e7159672c986e83629c104962e6b6e0e
Hyun Joon Lee <117090276+PPIONN@users.noreply.github.com>
2026-03-03 17:49:59 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	//ios_base::sync_with_stdio(false);
+	//cin.tie(0);
+
+	int A,B,Result;
+
+	cin >> A;
+	cin >> B;
+	Result = A + B;
+
+	cout << Result;
+
+}
