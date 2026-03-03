f6db53a8e63a6fca8e9c6ab4ab19d05b0e474eaf
Hyun Joon Lee <117090276+PPIONN@users.noreply.github.com>
2026-03-03 20:42:36 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+//#include <iomanip> // 소수점 출력 제한을 위함
+#include <string>
+//#include <cstring>
+//#include <algorithm>
+//#include <vector>
+using namespace std;
+
+
+ 
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	string str;
+
+
+	getline(cin,str);
+	cout << str << "??!";
+
+
+
+	return 0;
+
+}
