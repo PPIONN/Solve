fb69859eb81ba7b76252488613dd51da715b69c3
Hyun Joon Lee <117090276+PPIONN@users.noreply.github.com>
2026-03-18 09:19:07 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+    double answer = 0;
+    int check =0;
+    for(int i : numbers){
+        answer += i;
+    }
+    answer = answer/numbers.size();
+
+    return answer;
+}
