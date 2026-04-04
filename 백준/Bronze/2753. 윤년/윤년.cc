46a37e72df21b417c62325226c576872985f2ef9
Hyun Joon Lee <117090276+PPIONN@users.noreply.github.com>
2026-04-05 03:40:18 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.cc
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -1,24 +1,33 @@
 #include <iostream>
+
 using namespace std;
 
+
+
 int main() {
-	int a = 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
 
-	cin >> a;
-
-	if( a <= 4000 && a >=1 ){
-		if(a%4 == 0 && a%100 != 0){
-			printf("1");
+	int un = 0;
+	bool check = false;
+	cin >> un;
+	if (un % 4 == 0) {
+		check = true;
+		if (un % 100 == 0) {
+			check = false;
+			if (un % 400 == 0) {
+				check = true;
+			}
 		}
-		else if(a%100 == 0 && a%400 != 0){
-			printf("0");
-		}
-		else if(a%100 == 0 && a%400 == 0){
-			printf("1");
-		}
-
-		else(printf("0"));
-
 	}
+	
+	if (check == true) {
+		cout << 1;
+	}
+	else if (check == false){
+		cout << 0;
+	}
+
 	return 0;
+
 }
