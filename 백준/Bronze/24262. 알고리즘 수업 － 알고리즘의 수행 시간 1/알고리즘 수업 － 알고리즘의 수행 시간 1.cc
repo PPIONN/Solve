a20f39e5d1dd4160c66ccf209b6a4fa48164a15f
Hyun Joon Lee <117090276+PPIONN@users.noreply.github.com>
2026-04-13 07:57:22 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int num = 0;
+	cin >> num;
+	cout << 1 << "\n" << 0;
+
+
+	return 0;
+}
