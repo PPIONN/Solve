79ce202674df639aac62fddfaad9ccfdbd5813e8
Hyun Joon Lee <117090276+PPIONN@users.noreply.github.com>
2026-04-13 03:27:32 +0900

    [Bronze I] Title: 약수들의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	vector<int>vtr;
+	int num = 0;
+	while (cin >> num ) {
+		if (num == -1) break;
+		int result = 0;
+		for (int i = 1; i < num; i++) {
+			if (num % i == 0) {
+				vtr.push_back(i);
+			}
+		}
+		for (int i : vtr) {
+			result += i;
+		}
+		if (result == num) {
+			cout << result << " = 1";
+			for (int i : vtr) {
+				if(i != 1) cout << " + " << i;
+			}
+			cout << "\n";
+		}
+		else {
+			cout << num << " is NOT perfect." << "\n";
+		}
+		vtr.clear();
+	}
+
+
+
+	return 0;
+}
