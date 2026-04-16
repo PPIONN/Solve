65cb93993ff2d87fde0e5d19acb0b8b10b76ef16
Hyun Joon Lee <117090276+PPIONN@users.noreply.github.com>
2026-04-16 09:10:10 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.08 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -5,15 +5,5 @@ using namespace std;
 
 string solution(int n) {
     string answer = "";
-    if(n == 1){
-        answer += "수";
-        return answer;
-    }
-    for(int i =0; i<n/2;i++){
-        answer += "수박";
-    }
-    if(n%2 == 1){
-        answer +="수";
-    }
     return answer;
 }
